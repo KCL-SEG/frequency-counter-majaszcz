86ed0b8d7426323d98b2ee2f34687bf622e13411
Maja Szczepanczyk <majaszczepanczyk02@gmail.com>
2022-12-01 10:45:41 +0000

    first solution

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,8 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    items.sort()
+    for item in items:
+        frequencies.update({item : items.count(item)})
+    
     return frequencies
