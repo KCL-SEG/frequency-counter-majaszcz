b81a97d675eae3aea5c16fa673c9866a1efbb94d
Maja Szczepanczyk <majaszczepanczyk02@gmail.com>
2022-12-08 17:26:23 +0000

    implement frequency counter

--- frequencies.py
+++ frequencies.py
@@ -4,8 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    items.sort()
+    items = [str(x) for x in items]
+
     for item in items:
         frequencies.update({item : items.count(item)})
-    
+
     return frequencies
